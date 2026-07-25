81e4fba4be6ed6421e138a545b7b674a0efea7f2
NayzaLtd2006 <naylonstellenberg8@gmail.com>
2026-07-25 15:14:00 +0200

    Refactor navigation links and add 'How To Use' link

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 export default function App() {
+  const heroStyle = {position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "white"};
+
   return (
     <div style={{fontFamily: "Arial"}}>
-      
+
       <div style={{background: "#0ea5e9", color: "white", padding: "20px", textAlign: "center", position: "sticky", top: 0}}>
         <h1>SUNLINK HUB x SUN CITY</h1>
-        <a href="#marketplace" style={{color: "white", margin: "0 10px"}}>Marketplace</a>
-        <a href="#solar" style={{color: "white", margin: "0 10px"}}>Solar Shops</a>
-        <a href="#leaders" style={{color: "white", margin: "0 10px"}}>Hub Leaders</a>
-        <a href="#learn" style={{color: "white", margin: "0 10px"}}>Sunlink Learn</a>
-        <a href="#contact" style={{color: "white", margin: "0 10px"}}>Contact</a>
+        <a href="#marketplace" style={{color: "white", margin: "0 8px"}}>Marketplace</a>
+        <a href="#solar" style={{color: "white", margin: "0 8px"}}>Solar Shops</a>
+        <a href="#leaders" style={{color: "white", margin: "0 8px"}}>Hub Leaders</a>
+        <a href="#howto" style={{color: "white", margin: "0 8px"}}>How To Use</a>
+        <a href="#learn" style={{color: "white", margin: "0 8px"}}>Sunlink Learn</a>
+        <a href="#contact" style={{color: "white", margin: "0 8px"}}>Contact</a>
       </div>
 
-      <div style={{position: "relative", textAlign: "center"}}>
-        <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?w=1600" style={{width: "100%", height: "500px", objectFit: "cover", filter: "brightness(0.5)"}}/>
-        <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50
+      <div style={{position: "relative", textAlign: "center
